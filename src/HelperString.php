b86deefabcacd61eb02f6ada2837751f4ffd2f43
wycto <294287600@qq.com>
2021-12-05 00:12:55 +0800

    php不支持大括号数组更新

--- src/HelperString.php
+++ src/HelperString.php
@@ -121,7 +121,7 @@ class HelperString
         $key = NULL;
         $pattern = "1234567890abcdefghijklmnopqrstuvwxyz";
         for ($i = 0; $i < $length; $i ++) {
-            $key .= $pattern{rand(0, 35)};
+            $key .= $pattern[rand(0, 35)];
         }
         return $key;
     }
